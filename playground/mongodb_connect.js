97961aacc9c438033b86832ea2f170abeabfa188
Katy <ihatetoast@gmail.com>
2018-07-15 20:18:40 -0500

    redid some old playground activities

--- playground/mongodb_connect.js
+++ playground/mongodb_connect.js
@@ -1,44 +1,30 @@
 // const MongoClient = require('mongodb').MongoClient;
-
 const { MongoClient, ObjectID } = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/tutotodo', (error, client) => {
-  if (error) {
-    return console.log('Unable to connect to MongoDB server.');
-  }
-  console.log('Connected to MongoDB server.');
-  const db = client.db('TutotodosApp');
-  db.collection('Tutorials').insertOne(
-    {
-      title: 'CSS Grid',
-      author: 'Wes Bos',
-      source: 'https://cssgrid.io/',
-      keywords: ['CSS', 'grid', 'layout'],
-      body: 'Twenty-five videos and four hours to understand the new CSS Grid.',
-      completed: false
-    },
-    (error, result) => {
-      if (error) {
-        return console.log('unable to insert tutorial.', error);
-      }
-      console.log(JSON.stringify(result.ops, undefined, 2));
+//args: db where the todos live,, that silly obj option, then a callback fcn       err    db obj
+MongoClient.connect(
+  'mongodb://localhost:27017/tutotodo',
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      //return prevents the rest of the fcn from running
+
+      return console.log('Unable to connect to MongoDB server.');
     }
-  );
-
-  //insert new doc into the Users: name, age, location string
-  // db.collection('Users').insertOne(
-  //   {
-  //     name: 'The George Cassidy',
-  //     age: 4,
-  //     location: 'Austin, TX'
-  //   },
-  //   (error, result) => {
-  //     if (error) {
-  //       return console.log('Unable to insert user.');
-  //     }
-  //     console.log(result.ops[0]._id.getTimestamp());
-  //   }
-  // );
-
-  client.close();
-});
+    console.log('Connected to MongoDB server.');
+    const db = client.db('tutotodo')
+    db.collection('tutorials').insertOne({
+      title: 'ES6 for Everyone',
+      completed: false,
+      tutor: 'Wes Bos',
+      keywords: ['ES6', 'JavaScript']
+    }, (err, result) => {
+      if (err) {
+        return console.log("Oops. Couldn't enter tutorial", err);
+      }
+      //pretty
+      console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    })
+    client.close();
+  }
+);
--- playground/mongodb_find.js
+++ playground/mongodb_find.js
@@ -1,53 +1,38 @@
-// const MongoClient = require('mongodb').MongoClient;
-
 const { MongoClient, ObjectID } = require('mongodb');
-//_id: new ObjectID('5b0b56d12b4a3a25226f3163')
+//db is the tutotodo
+//collection is tutorials
 
-MongoClient.connect('mongodb://localhost:27017/tutotodo', (error, client) => {
+MongoClient.connect('mongodb://localhost:27017/tutotodo', { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to MongoDB server.');
   }
   console.log('Connected to MongoDB server.');
-  const db = client.db('TutotodosApp');
+  const db = client.db('tutotodo');
   // db
-  //   .collection('Tutorials')
-  //   .find({
-  //     _id: new ObjectID('5b0b56d12b4a3a25226f3163')
-  //   })
-  //   .toArray()
+  //   .collection('tutorials')
+  //   .find({ _id: new ObjectID('5b4bd217d2b7937fc985a7a8') })
+  //   // .find()
+  //   .toArray()//returns a promise
   //   .then(
   //     docs => {
-  //       console.log('Tutorials');
+  //       console.log('found by id');
   //       console.log(JSON.stringify(docs, undefined, 2));
   //     },
   //     err => {
-  //       console.log('unable to fetch tutorials', err);
+  //       console.log('unable to find by id', err);
   //     }
   //   );
-  // db
-  //   .collection('Tutorials')
-  //   .find()
-  //   .count()
-  //   .then(
-  //     count => {
-  //       console.log(`Tutorials count: ${count}`);
-  //     },
-  //     err => {
-  //       console.log('unable to fetch tutorials', err);
-  //     }
-  //   );
-
   db
-    .collection('Users')
-    .find({ name: 'Fabian Cassidy' })
-    .toArray()
+    .collection('tutorials')
+    .find({ tutor: 'Wes Bos' })
+    // .find()
+    .count()//returns a promise
     .then(
-      docs => {
-        console.log('Docs with name of Fabian');
-        console.log(JSON.stringify(docs, undefined, 2));
+      count => {
+        console.log(`Tutorials by Wes Bos: ${count}`);
       },
       err => {
-        console.log('unable to find faby', err);
+        console.log('unable to find jack doody', err);
       }
     );
   // client.close();
